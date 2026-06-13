7f5a64d021e10e81f6d06eb46a64787174d606bf
sudharsan006-cloud <ssan28252@gmail.com>
2026-06-13 16:48:55 +0530

    fix: global missing SkeletonTable imports

--- frontend/src/pages/Loans.js
+++ frontend/src/pages/Loans.js
@@ -1,3 +1,4 @@
+import { SkeletonTable } from '../components/SkeletonCard';
 import React, { useState, useEffect } from 'react';
 import { PageWrapper, AnimatedItem } from '../components/PageWrapper';
 import API from '../api';
--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -1,3 +1,4 @@
+import { SkeletonTable } from '../components/SkeletonCard';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import API from '../api';
